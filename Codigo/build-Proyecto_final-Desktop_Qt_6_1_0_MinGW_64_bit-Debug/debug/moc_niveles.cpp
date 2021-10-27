d8dd583338c05a7ec538f76699c9e8c14cf543e5
Jesus-Ibarra <80048825+Jesus-Ibarra@users.noreply.github.com>
2021-10-27 16:40:20 -0500

    comenzando el disparo

--- Codigo/build-Proyecto_final-Desktop_Qt_6_1_0_MinGW_64_bit-Debug/debug/moc_niveles.cpp
+++ Codigo/build-Proyecto_final-Desktop_Qt_6_1_0_MinGW_64_bit-Debug/debug/moc_niveles.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_niveles_t {
-    const uint offsetsAndSize[16];
-    char stringdata0[92];
+    const uint offsetsAndSize[18];
+    char stringdata0[103];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(offsetof(qt_meta_stringdata_niveles_t, stringdata0) + ofs), len 
@@ -36,12 +36,13 @@ QT_MOC_LITERAL(20, 10), // "Movim_per3"
 QT_MOC_LITERAL(31, 12), // "temporizador"
 QT_MOC_LITERAL(44, 6), // "tempor"
 QT_MOC_LITERAL(51, 19), // "on_anterior_clicked"
-QT_MOC_LITERAL(71, 20) // "on_siguiente_clicked"
+QT_MOC_LITERAL(71, 20), // "on_siguiente_clicked"
+QT_MOC_LITERAL(92, 10) // "actualizar"
 
     },
     "niveles\0Movim_per2\0\0Movim_per3\0"
     "temporizador\0tempor\0on_anterior_clicked\0"
-    "on_siguiente_clicked"
+    "on_siguiente_clicked\0actualizar"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,7 +52,7 @@ static const uint qt_meta_data_niveles[] = {
        9,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -59,14 +60,16 @@ static const uint qt_meta_data_niveles[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   50,    2, 0x08,    0 /* Private */,
-       3,    0,   51,    2, 0x08,    1 /* Private */,
-       4,    0,   52,    2, 0x08,    2 /* Private */,
-       5,    0,   53,    2, 0x08,    3 /* Private */,
-       6,    0,   54,    2, 0x08,    4 /* Private */,
-       7,    0,   55,    2, 0x08,    5 /* Private */,
+       1,    0,   56,    2, 0x08,    0 /* Private */,
+       3,    0,   57,    2, 0x08,    1 /* Private */,
+       4,    0,   58,    2, 0x08,    2 /* Private */,
+       5,    0,   59,    2, 0x08,    3 /* Private */,
+       6,    0,   60,    2, 0x08,    4 /* Private */,
+       7,    0,   61,    2, 0x08,    5 /* Private */,
+       8,    0,   62,    2, 0x08,    6 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -89,6 +92,7 @@ void niveles::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, voi
         case 3: _t->tempor(); break;
         case 4: _t->on_anterior_clicked(); break;
         case 5: _t->on_siguiente_clicked(); break;
+        case 6: _t->actualizar(); break;
         default: ;
         }
     }
@@ -103,7 +107,7 @@ const QMetaObject niveles::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_niveles_t
 
-, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -130,13 +134,13 @@ int niveles::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 7)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 6;
+        _id -= 7;
     }
     return _id;
 }
